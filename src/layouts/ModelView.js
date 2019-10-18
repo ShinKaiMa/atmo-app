fa47ae099c52726e9c9b3c9d75926329529c9725
Shinkai <shinkaivin1993@gmail.com>
2019-10-18 21:13:07 +0800

    Bottom navbar (in dev) part2

--- src/layouts/ModelView.js
+++ src/layouts/ModelView.js
@@ -11,8 +11,9 @@ import useWindowSize from "../hooks/useWindowSize";
 
 const ModelView = props => {
   const [width, height] = useWindowSize();
-  console.log(`width: ${width}`);
   useEffect(() => {
+    console.log(`width: ${width}`);
+    console.log(`height: ${height}`);
     // let slider = document.getElementById("slider");
     // noUiSlider.create(slider, {
     //   connect: true,
@@ -47,11 +48,19 @@ const ModelView = props => {
     // var tabs = document.querySelectorAll(".tabs");
     // var instance = M.Tabs.init(tabs);
     var dropdowns = document.querySelectorAll(".dropdown-trigger");
-    var dropdownsInstances = M.Dropdown.init(dropdowns,{
-      alignment:'center',
-      inDuration: 500,
-    });
-  }, []);
+    console.log(`width: ${width}`)
+    console.log(`hover: ${width >= 1024}`)
+    if (width !== 0) {
+      var dropdownsInstances = M.Dropdown.init(dropdowns, {
+        hover: width > 1080 ? true : false,
+        inDuration: 500,
+        constrainWidth: true
+      });
+
+      var tabs = document.querySelectorAll(".tabs");
+      var instance = M.Tabs.init(tabs);
+    }
+  }, [width, height]);
 
   return (
     <div>
@@ -138,263 +147,84 @@ const ModelView = props => {
           tenetur? Fuga ipsum repudiandae neque dicta temporibus harum. Facilis
           impedit iusto consectetur?
         </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
-          praesentium illo ut, esse exercitationem nulla sunt natus dignissimos
-          tenetur? Fuga ipsum repudiandae neque dicta temporibus harum. Facilis
-          impedit iusto consectetur?
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
-          praesentium illo ut, esse exercitationem nulla sunt natus dignissimos
-          tenetur? Fuga ipsum repudiandae neque dicta temporibus harum. Facilis
-          impedit iusto consectetur?
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
-          praesentium illo ut, esse exercitationem nulla sunt natus dignissimos
-          tenetur? Fuga ipsum repudiandae neque dicta temporibus harum. Facilis
-          impedit iusto consectetur?
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
-          praesentium illo ut, esse exercitationem nulla sunt natus dignissimos
-          tenetur? Fuga ipsum repudiandae neque dicta temporibus harum. Facilis
-          impedit iusto consectetur?
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
-          praesentium illo ut, esse exercitationem nulla sunt natus dignissimos
-          tenetur? Fuga ipsum repudiandae neque dicta temporibus harum. Facilis
-          impedit iusto consectetur?
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
-          praesentium illo ut, esse exercitationem nulla sunt natus dignissimos
-          tenetur? Fuga ipsum repudiandae neque dicta temporibus harum. Facilis
-          impedit iusto consectetur?
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
-          praesentium illo ut, esse exercitationem nulla sunt natus dignissimos
-          tenetur? Fuga ipsum repudiandae neque dicta temporibus harum. Facilis
-          impedit iusto consectetur?
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
-          praesentium illo ut, esse exercitationem nulla sunt natus dignissimos
-          tenetur? Fuga ipsum repudiandae neque dicta temporibus harum. Facilis
-          impedit iusto consectetur?
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
-          praesentium illo ut, esse exercitationem nulla sunt natus dignissimos
-          tenetur? Fuga ipsum repudiandae neque dicta temporibus harum. Facilis
-          impedit iusto consectetur?
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
-          praesentium illo ut, esse exercitationem nulla sunt natus dignissimos
-          tenetur? Fuga ipsum repudiandae neque dicta temporibus harum. Facilis
-          impedit iusto consectetur?
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
-          praesentium illo ut, esse exercitationem nulla sunt natus dignissimos
-          tenetur? Fuga ipsum repudiandae neque dicta temporibus harum. Facilis
-          impedit iusto consectetur?
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
-          praesentium illo ut, esse exercitationem nulla sunt natus dignissimos
-          tenetur? Fuga ipsum repudiandae neque dicta temporibus harum. Facilis
-          impedit iusto consectetur?
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
-          praesentium illo ut, esse exercitationem nulla sunt natus dignissimos
-          tenetur? Fuga ipsum repudiandae neque dicta temporibus harum. Facilis
-          impedit iusto consectetur?
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
-          praesentium illo ut, esse exercitationem nulla sunt natus dignissimos
-          tenetur? Fuga ipsum repudiandae neque dicta temporibus harum. Facilis
-          impedit iusto consectetur?
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
-          praesentium illo ut, esse exercitationem nulla sunt natus dignissimos
-          tenetur? Fuga ipsum repudiandae neque dicta temporibus harum. Facilis
-          impedit iusto consectetur?
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
-          praesentium illo ut, esse exercitationem nulla sunt natus dignissimos
-          tenetur? Fuga ipsum repudiandae neque dicta temporibus harum. Facilis
-          impedit iusto consectetur?
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
-          praesentium illo ut, esse exercitationem nulla sunt natus dignissimos
-          tenetur? Fuga ipsum repudiandae neque dicta temporibus harum. Facilis
-          impedit iusto consectetur?
-        </p>
-        {/* <div
-        className="mainLayout container"
-        id="container"
-        style={{
-          position: "fixed",
-          bottom: "0",
-          left:"0px",right:"0px",
-          // left:width,
-          // marginLeft: "20px",
-          // marginBottom:"0px",
-          marginTop:"0",
-          marginBottom:"0",
-          marginLeft:"0",
-          marginRight:"0",
-          padding:"0px",
-          paddingLeft:"180px",
-          width: width-180,
-          height: "45px",
-          zIndex: "999"
-        }} */}
-      >
         <div class="row"
-        style={{
-          position: "fixed",
-          bottom: "0",
-          left:"0px",right:"0px",
-          // left:width,
-          // marginLeft: "20px",
-          // marginBottom:"0px",
-          marginTop:"0",
-          marginBottom:"0",
-          marginLeft:"0",
-          marginRight:"0",
-          // padding:"0px",
-          paddingLeft:"200px",
-          width: width,
-          height: "45px",
-          zIndex: "995"
-        }}>
-          <a
-            class="dropdown-trigger btn col s4"
-            data-target="dropdown"
-            style={{ height: "45px", backgroundColor:"white", color:"#0ACAF5", borderRadius:"0px", border:"0px" }}
-          >
-            Drop Me!
-          </a>
-          <a
-            class="dropdown-trigger btn col s4"
-            data-target="dropdown"
-            style={{ height: "45px", backgroundColor:"white", color:"#0ACAF5", borderRadius:"0px", border:"0px"}}
-          >
-            Drop Me!
-          </a>
-          <a
-            class="dropdown-trigger btn col s4"
-            data-target="dropdown"
-            style={{ height: "45px", backgroundColor:"white", color:"#0ACAF5", borderRadius:"0px", border:"0px"}}
-          >
-            Drop Me!
-          </a>
-          <ul id="dropdown" class="dropdown-content collection" style={{ zIndex: "999" }}>
-            <li className="collection-item">
-              <a >one</a>
-            </li>
-            <li  className="collection-item">
-              <a>two</a>
-            </li>
-            <li  className="collection-item">
-              <a>three</a>
-            </li>
-          </ul>
-        </div>
-      {/* </div> */}
-      </div>
-      
-      {/* <div
-        class="mainLayout container"
-        id="container"
-        style={{
-          position: "fixed",
-          bottom: "0",
-          margin: "0px",
-          width: "100%",
-          zIndex:"500"
-        }}
-      >
-          <nav class="nav-extended">
-            <div class="nav-content">
-              <ul class="tabs tabs-transparent row">
-                <li class="tab col s4">
-                  <a style={{textTransform:"none"}}>Precipitation</a>
-                </li>
-                <li class="tab col s4">
-                  <a style={{textTransform:"none"}}>
-                    Wind
-                  </a>
-                </li>
-                <li class="tab col s4">
-                  <a  style={{textTransform:"none"}}>Temperature</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-      </div> */}
+          style={{
+            position: "fixed",
+            bottom: "0",
+            left: "0px", right: "0px",
+            // left:width,
+            // marginLeft: "20px",
+            // marginBottom:"0px",
+            marginTop: "0",
+            marginBottom: "0",
+            marginLeft: "0",
+            marginRight: "0",
+            // padding:"0px",
+            paddingLeft: width > 993 ? "200px" : "0px",
+            width: width > 993 ? width : "100%",
+            height: "45px",
+            zIndex: "998"
+          }}>
+          <div className="bottomNav" style={{ height: "45px" }}>
 
-      {/* <div
-        class="mainLayout container"
-        id="container"
-        style={{
-          position: "fixed",
-          bottom: "0",
-          margin: "0px",
-          width: "100%",
-          height: "45px",
-          zIndex:"999"
-        }}
-      >
-        <div class="row">
-          <a
-            class="dropdown-trigger btn col s4"
-            data-target="dropdown1"
-            style={{ height: "45px", padding:"0px" }}
-          >
-            Drop Me!
-          </a>
-          <a
-            class="dropdown-trigger btn col s4"
-            data-target="dropdown1"
-            style={{ height: "45px", padding:"0px" }}
-          >
-            Drop Me!
-          </a>
-          <a
-            class="dropdown-trigger btn col s4"
-            data-target="dropdown1"
-            style={{ height: "45px", padding:"0px" }}
-          >
-            Drop Me!
-          </a>
-        </div>
-      </div>
 
-      <div className="mainLayout container" id="container">
-      <ul id="dropdown1" class="dropdown-content" style={{zIndex:"999"}}>
-        <li>
-          <a href="#!">one</a>
-        </li>
-        <li>
-          <a href="#!">two</a>
-        </li>
-        <li>
-          <a href="#!">three</a>
-        </li>
-      </ul>
-      </div> */}
+            <a
+              class="dropdown-trigger btn col s4"
+              data-target="dropdown1"
+              style={{ height: "45px", backgroundColor: "white", color: "#0ACAF5", borderRadius: "0px", border: "90px" }}
+            >
+              Drop Me!
+          </a>
+            <a
+              class="dropdown-trigger btn col s4"
+              data-target="dropdown2"
+              style={{ height: "45px", backgroundColor: "white", color: "#0ACAF5", borderRadius: "0px", border: "0px" }}
+            >
+              Drop Me!
+          </a>
+            <a
+              class="dropdown-trigger btn col s4"
+              data-target="dropdown3"
+              style={{ height: "45px", backgroundColor: "white", color: "#0ACAF5", borderRadius: "0px", border: "0px" }}
+            >
+              Drop Me!
+          </a>
+          </div>
+        </div>
+        <ul id="dropdown1" class="dropdown-content collection" style={{ zIndex: "990" }}>
+          <li className="collection-item">
+            <a >one</a>
+          </li>
+          <li className="collection-item">
+            <a>two</a>
+          </li>
+          <li className="collection-item">
+            <a>three</a>
+          </li>
+        </ul>
+        <ul id="dropdown2" class="dropdown-content collection" style={{ zIndex: "990" }}>
+          <li className="collection-item">
+            <a >one</a>
+          </li>
+          <li className="collection-item">
+            <a>two</a>
+          </li>
+          <li className="collection-item">
+            <a>three</a>
+          </li>
+        </ul>
+        <ul id="dropdown3" class="dropdown-content collection" style={{ zIndex: "990" }}>
+          <li className="collection-item">
+            <a >one</a>
+          </li>
+          <li className="collection-item">
+            <a>two</a>
+          </li>
+          <li className="collection-item">
+            <a>three</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
