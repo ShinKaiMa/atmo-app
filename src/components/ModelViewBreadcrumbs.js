c0292bad5ca4423c8cec303fdad50e83a86be549
ShinKai <shinkaivin1993@gmail.com>
2019-10-22 00:50:14 +0800

    Complete. area selector in model view breadcrumbs

--- src/components/ModelViewBreadcrumbs.js
+++ src/components/ModelViewBreadcrumbs.js
@@ -2,79 +2,142 @@ import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import ModelViewAreaSelector from "../components/ModelViewAreaSelector";
 import M from "materialize-css/dist/js/materialize.min.js";
+import useWindowSize from "../hooks/useWindowSize";
 
-const ModelViewBreadcrumbs = (props) => {
-    const [breadcrumbs, setBreadcrumbs] = useState();
+const ModelViewBreadcrumbs = props => {
+  const [breadcrumbs, setBreadcrumbs] = useState();
+  const [area, setArea] = useState();
+  const [width, height] = useWindowSize();
+  const THRESHOLD_WIDTH_PIXEL = 500;
 
-    useEffect(() => {
-        if (
-            !props.match &&
-            !props.match.params &&
-            !props.match.params.area &&
-            !props.match.params.type &&
-            !props.match.params.model
-        ) {
-            props.history.push("/");
-        } else {
-            let newBreadcrumbs = [];
+  useEffect(() => {
+    if (
+      !props.match &&
+      !props.match.params &&
+      !props.match.params.area &&
+      !props.match.params.type &&
+      !props.match.params.model
+    ) {
+      props.history.push("/");
+    } else {
+      let newBreadcrumbs = [];
 
-            if (props.match.params.area === "global") {
-                newBreadcrumbs.push("Global Model");
-            } else if (props.match.params.area === "regional") {
-                newBreadcrumbs.push("Regional Model");
-            }
+      if (props.match.params.area === "global") {
+        newBreadcrumbs.push("Global Model");
+      } else if (props.match.params.area === "regional") {
+        newBreadcrumbs.push("Regional Model");
+      }
 
-            if (props.match.params.model === "GFS") {
-                newBreadcrumbs.push("GFS");
-            } else if (props.match.params.model === "ECMWF") {
-                newBreadcrumbs.push("ECMWF");
-            } else if (props.match.params.model === "CWBWRF") {
-                newBreadcrumbs.push("CWB WRF");
-            }
+      if (props.match.params.model === "GFS") {
+        newBreadcrumbs.push("GFS");
+      } else if (props.match.params.model === "ECMWF") {
+        newBreadcrumbs.push("ECMWF");
+      } else if (props.match.params.model === "CWBWRF") {
+        newBreadcrumbs.push("CWB WRF");
+      }
 
-            if (newBreadcrumbs.length !== 2) {
-                props.history.push("/");
-            } else {
-                setBreadcrumbs(newBreadcrumbs);
-            }
-        }
-        var elems = document.querySelectorAll('.dropdown-trigger.area');
-        var instances = M.Dropdown.init(elems, {
-            coverTrigger: false
-        });
-    }, [props]);
+      if (newBreadcrumbs.length !== 2) {
+        props.history.push("/");
+      } else {
+        setBreadcrumbs(newBreadcrumbs);
+      }
+    }
+    var elems = document.querySelectorAll(".dropdown-trigger.area");
+    var instances = M.Dropdown.init(elems, {
+      coverTrigger: false
+    });
+  }, [props]);
 
-
-    return (
-        <nav className="z-depth-0 col s12" style={{ height: "20px", lineHeight: "20px", marginBottom: "20px" }}>
-            <div className="nav-wrapper " style={{ height: "20px", paddingTop: "10px" }}>
-                <div style={{ height: "20px" }}>
-                    <a className="breadcrumb pointer" onClick={() => props.history.push('/')}>
-                        <i className="material-icons" style={{ color: "rgba(50,50,50,0.4)", lineHeight: "20px", fontSize: "21px" }}>home</i>
-                    </a>
-                    <a className="breadcrumb" style={{ fontSize: "18px" }}>
-                        {breadcrumbs ? breadcrumbs[0] : ""}
-                    </a>
-                    <a className="breadcrumb active" style={{ fontSize: "18px" }}>
-                        {breadcrumbs ? breadcrumbs[1] : ""}
-                    </a>
-                    <a class='breadcrumb'>
+  return (
+    <nav
+      className="z-depth-0 col s12"
+      style={{ height: "20px", lineHeight: "20px", marginBottom: "20px" }}
+    >
+      <div
+        className="nav-wrapper "
+        style={{ height: "20px", paddingTop: "10px" }}
+      >
+        <div style={{ height: "20px" }}>
+          <a
+            className="breadcrumb pointer"
+            onClick={() => props.history.push("/")}
+          >
+            {/* <i className="material-icons" style={{ color: "rgba(50,50,50,0.4)", lineHeight: "20px", fontSize: "21px" }}>home</i> */}
+            <i
+              className="material-icons"
+              style={{
+                color: "rgba(50,50,50,0.4)",
+                lineHeight: width > THRESHOLD_WIDTH_PIXEL ? "20px" : "20px",
+                fontSize: width > THRESHOLD_WIDTH_PIXEL ? "21px" : "21px"
+              }}
+            >
+              home
+            </i>
+          </a>
+          {/* <a className="breadcrumb" style={{ fontSize: "18px" }}> */}
+          <a
+            className="breadcrumb"
+            style={{
+              fontSize: width > THRESHOLD_WIDTH_PIXEL ? "18px" : "10px"
+            }}
+          >
+            {breadcrumbs ? breadcrumbs[0] : ""}
+          </a>
+          {/* <a className="breadcrumb active" style={{ fontSize: "18px" }}> */}
+          <a
+            className="breadcrumb active"
+            style={{
+              fontSize: width > THRESHOLD_WIDTH_PIXEL ? "18px" : "10px"
+            }}
+          >
+            {breadcrumbs ? breadcrumbs[1] : ""}
+          </a>
+          {/* <a class='breadcrumb'>
                         <a class='dropdown-trigger btn area' data-target='area' style={{ backgroundColor: "transparent", color: "gray", height: "25px", lineHeight: "20px" }}>
                         <i class="material-icons right" style={{lineHeight: "25px"}}>keyboard_arrow_down</i>Area
                     </a>
-                    </a>
-                </div>
-            </div>
-            <ul id='area' class='dropdown-content'>
-                <li><a href="#!">one</a></li>
-                <li><a href="#!">two</a></li>
-                <li class="divider" tabindex="-1"></li>
-                <li><a href="#!">three</a></li>
-                <li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
-                <li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
-            </ul>
-        </nav>
-    );
-}
+                    </a> */}
+          <a class="breadcrumb">
+            <a
+              class="dropdown-trigger btn area"
+              data-target="area"
+              style={{
+                backgroundColor: "transparent",
+                width: width > THRESHOLD_WIDTH_PIXEL ? "120px" : "70px",
+                color: "gray",
+                height: width > THRESHOLD_WIDTH_PIXEL ? "25px" : "20px",
+                lineHeight: width > THRESHOLD_WIDTH_PIXEL ? "20px" : "18px",
+                fontSize: width > THRESHOLD_WIDTH_PIXEL ? "18px" : "10px",
+                padding: "0 0 0 3px",
+                color:"#0ACAF5"
+              }}
+            >
+              <i
+                class="material-icons right"
+                style={{
+                  lineHeight: width > THRESHOLD_WIDTH_PIXEL ? "21px" : "20px",
+                  fontSize: "13px",
+                  color:"gray"
+                }}
+              >
+                keyboard_arrow_down
+              </i>
+              Area
+            </a>
+          </a>
+        </div>
+      </div>
+      <ul id="area" class="dropdown-content area">
+        <li>
+          <a style={{fontSize: width > THRESHOLD_WIDTH_PIXEL ? "16px" : "10px",}}>Near Taiwan</a>
+        </li>
+        <li class="divider" tabindex="-1"></li>
+        <li>
+          <a style={{fontSize: width > THRESHOLD_WIDTH_PIXEL ? "16px" : "10px",}}>Taiwan</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default withRouter(ModelViewBreadcrumbs)
+export default withRouter(ModelViewBreadcrumbs);
